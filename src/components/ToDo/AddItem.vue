21c3dff4289bf4e0d8721f716986a204d535e47c
Dastan Akhmetov <ddakhmet@gmail.com>
2018-03-03 01:47:21 +0600

    main players

---
+++ src/components/ToDo/AddItem.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="columns">
+    <div class="column is-half is-offset-one-quarter">
+      <div class="field has-addons">
+        <div class="control is-expanded has-icons-left">
+          <input class="input is-medium"
+                 type="text"
+                 placeholder="Type your task..."
+                 v-model="item.title"
+                 @keyup.enter="onAdd"
+                 ref="input"
+          >
+          <span class="icon is-medium is-left">
+            <FontAwesomeIcon icon="tasks"/>
+            <!--<i class="fas fa-tasks"></i>-->
+          </span>
+        </div>
+        <div class="control">
+          <a class="button is-medium is-success" @click="onAdd">
+            Do It!&nbsp;
+            <i class="fas fa-caret-right"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+
+export default {
+  props: {
+    onChange: {
+      type: Function,
+      required: true
+    }
+  },
+  components: {
+    FontAwesomeIcon
+  },
+  data () {
+    return {
+      item: {
+        id: 1,
+        title: '',
+        priority: 'Normal'
+      }
+    }
+  },
+  mounted () {
+    console.log(this.$refs.input)
+    this.$refs.input.focus()
+  },
+  methods: {
+    onAdd () {
+      if (this.item.title.length > 0) {
+        const item = {
+          id: this.item.id++,
+          title: this.item.title,
+          priority: this.item.priority,
+          date: Date.now(),
+          isDone: false
+        }
+        this.onChange(item)
+        this.flushItem()
+      }
+    },
+    flushItem () {
+      this.item.title = ''
+      this.item.priority = 'Normal'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .column {
+    margin-top: 20px
+  }
+</style>
---
+++ src/components/ToDo/List.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="columns">
+    <div class="column is-10 is-offset-1">
+      <table class="table is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th width="60%">Title</th>
+            <th width="20%">Date</th>
+            <th>Priority</th>
+            <th width="15%" class="centered">Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tfoot>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Date</th>
+            <th>Priority</th>
+            <th class="centered">Status</th>
+            <th>Actions</th>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr v-for="item in todoItems"
+              :key="item.id"
+          >
+            <th>{{ item.id }}</th>
+            <td>{{ item.title }}</td>
+            <td>{{ item.formattedDate }}</td>
+            <td>
+              <span class="tag" :class="item.priorityClass">
+                {{ item.priority }}
+              </span>
+            </td>
+            <td style="text-align: center;">
+              <Status :item="item"/>
+            </td>
+            <td>
+              <Dropdown :item="item" :on-mark-as-done="onChange"/>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import { DateTime } from 'luxon'
+import Status from '../Status'
+import Dropdown from '../Dropdown'
+
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    onChange: {
+      type: Function,
+      required: true
+    },
+    onDelete: {
+      type: Function,
+      required: true
+    }
+  },
+  components: {
+    Status,
+    Dropdown
+  },
+  computed: {
+    todoItems () {
+      return this.items.map(i => {
+        i.formattedDate = DateTime.fromMillis(i.date).toFormat('dd-MM-yyyy TT')
+        i.priorityClass = ''
+        switch (i.priority) {
+          case 'Normal' : i.priorityClass = 'is-success'; break
+          case 'High' : i.priorityClass = 'is-danger'; break
+          case 'Low': i.priorityClass = 'is-warning'; break
+          default : i.priorityClass = 'is-success'
+        }
+        return i
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .centered {
+    text-align: center;
+  }
+</style>
