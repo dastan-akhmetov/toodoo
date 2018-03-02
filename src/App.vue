378a4346dc6e49ea3c68751e403f26841fefe237
Dastan Akhmetov <ddakhmet@gmail.com>
2018-03-03 01:47:02 +0600

    main functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header></Header>
+    <div class="container">
+      <AddToDoItem :on-change="onAddToDoItem" />
+      <TabLists :on-change="onChangeList" />
+      <ToDoList :items="filteredTodoItems" :on-change="onChangeItem" :on-delete="onDeleteItem" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import TabLists from './components/TabLists'
+import AddToDoItem from './components/ToDo/AddItem'
+import ToDoList from './components/ToDo/List'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    TabLists,
+    AddToDoItem,
+    ToDoList
+  },
+  data () {
+    return {
+      todoItems: [],
+      currentList: 'All'
+    }
+  },
+  computed: {
+    filteredTodoItems () {
+      return this.todoItems.filter(i => {
+        if (this.currentList === 'All') return i
+        else if (this.currentList === 'In Progress') return i.isDone !== true
+        else if (this.currentList === 'Finished') return i.isDone === true
+      })
+    }
+  },
+  methods: {
+    onChangeList (name) {
+      this.currentList = name
+    },
+    onAddToDoItem (item) {
+      this.todoItems.unshift(item)
+    },
+    onChangeItem (item) {
+      this.todoItems = this.todoItems.map(ti => {
+        if (ti.id === item.id) {
+          ti = item
+        }
+        return ti
+      })
+    },
+    onDeleteItem (item) {
+      const found = this.todoItems.find(i => i.id === item.it)
+      const indexOf = this.todoItems.indexOf(found)
+      this.todoItems.splice(indexOf, 1)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
