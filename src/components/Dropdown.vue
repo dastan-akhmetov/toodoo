f0f116c8ea6240374243f2acc4eeeb5d44cd95df
Dastan Akhmetov <ddakhmet@gmail.com>
2018-03-03 05:07:06 +0600

    save, edit

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="dropdown-menu" id="dropdown-menu" role="menu">
       <div class="dropdown-content">
-        <a href="#" class="dropdown-item" @click="onMarkItemAsDoneOrNot">
+        <a v-if="!isEditing" class="dropdown-item" @click="onMarkItemAsDoneOrNot">
           <span v-if="!item.isDone">
             <FontAwesomeIcon icon="check-circle"/> Mark as done
           </span>
@@ -18,10 +18,15 @@
             <FontAwesomeIcon icon="ban"/> Mark as undone
           </span>
         </a>
-        <a class="dropdown-item" @click="onEditItem">
-          <FontAwesomeIcon icon="edit"/> Edit
+        <a class="dropdown-item">
+          <div v-if="isEditing" @click="onSaveEdit">
+            <FontAwesomeIcon icon="check"/> Save
+          </div>
+          <div v-else @click="onEditItem">
+            <FontAwesomeIcon icon="edit"/> Edit
+          </div>
         </a>
-        <a href="#" class="dropdown-item" @click="onDeleteItem">
+        <a v-if="!isEditing" class="dropdown-item" @click="onDeleteItem">
           <FontAwesomeIcon icon="trash"/> Delete
         </a>
       </div>
@@ -31,6 +36,8 @@
 
 <script>
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import { EventBus } from './EventBus'
+
 export default {
   props: {
     item: {
@@ -51,7 +58,8 @@ export default {
   },
   data () {
     return {
-      className: ''
+      className: '',
+      isEditing: false
     }
   },
   methods: {
@@ -65,25 +73,32 @@ export default {
       else this.markAsUndoneToast(item.title)
       this.toggle()
     },
-    markAsDoneToast(title) {
+    markAsDoneToast (title) {
       this.$toast.open({
         message: `Item "${title}" marked as done!`,
         type: 'is-success'
       })
     },
-    markAsUndoneToast(title) {
+    markAsUndoneToast (title) {
       this.$toast.open({
         message: `Item "${title}" marked as undone!`,
         type: 'is-danger'
       })
     },
     onEditItem () {
+      EventBus.$emit('ItemEditing.Init', this.item.id)
+      this.isEditing = true
+      this.toggle()
+    },
+    onSaveEdit () {
+      EventBus.$emit('ItemEditing.Finished', this.item.id)
+      this.isEditing = false
       this.toggle()
     },
     onDeleteItem () {
       this.confirmDelete(this.item.title)
     },
-    confirmDelete(title) {
+    confirmDelete (title) {
       this.$dialog.confirm({
         title: 'Deleting item',
         message: `Are you sure you want to delete the item <b>"${title}"</b>? This action cannot be undone.`,
--- src/components/ToDo/TableList.vue
+++ src/components/ToDo/TableList.vue
@@ -6,8 +6,7 @@
           <tr>
             <th>#</th>
             <th width="60%">Title</th>
-            <th width="20%">Date</th>
-            <th>Priority</th>
+            <th width="10%">Due</th>
             <th width="15%" class="centered">Status</th>
             <th>Actions</th>
           </tr>
@@ -16,8 +15,7 @@
           <tr>
             <th>#</th>
             <th>Title</th>
-            <th>Date</th>
-            <th>Priority</th>
+            <th>Due</th>
             <th class="centered">Status</th>
             <th>Actions</th>
           </tr>
@@ -25,11 +23,20 @@
         <tbody>
           <tr v-for="item in todoItems" :key="item.id">
             <th>{{ item.id }}</th>
-            <td>{{ item.title }}</td>
-            <td>{{ item.formattedDate }}</td>
             <td>
-              <span class="tag" :class="item.priorityClass">
-                {{ item.priority }}
+              <template v-if="itemEditingId === item.id && isEditing">
+                <b-input v-model="item.title"></b-input>
+              </template>
+              <template v-else>
+                {{ item.title }}
+              </template>
+            </td>
+            <td>
+              <span v-if="itemEditingId === item.id && isEditing">
+                <b-datepicker v-model="changingDate"></b-datepicker>
+              </span>
+              <span v-else class="tag" :class="item.emergencyLevel" :title="item.emergencyLevelTitle">
+                {{ item.formattedDueDate }}
               </span>
             </td>
             <td style="text-align: center;">
@@ -49,6 +56,7 @@
 import { DateTime } from 'luxon'
 import Status from '../Status'
 import Dropdown from '../Dropdown'
+import { EventBus } from '../EventBus'
 
 export default {
   props: {
@@ -69,16 +77,50 @@ export default {
     Status,
     Dropdown
   },
+  created () {
+    EventBus.$on('ItemEditing.Init', id => {
+      this.itemEditingId = id
+      this.isEditing = true
+      const item = this.todoItems.find(t => t.id === id)
+      this.changingDate = new Date(item.dueDate)
+    })
+    EventBus.$on('ItemEditing.Finished', id => {
+      this.isEditing = false
+      let item = this.todoItems.find(t => t.id === id)
+      item.dueDate = this.changingDate.getTime()
+      this.onChange(item)
+    })
+  },
+  data () {
+    return {
+      itemEditingId: null,
+      isEditing: false,
+      changingDate: new Date()
+    }
+  },
   computed: {
     todoItems () {
       return this.items.map(i => {
-        i.formattedDate = DateTime.fromMillis(i.date).toFormat('ff')
-        i.priorityClass = ''
-        switch (i.priority) {
-          case 'Normal' : i.priorityClass = 'is-success'; break
-          case 'High' : i.priorityClass = 'is-danger'; break
-          case 'Low': i.priorityClass = 'is-warning'; break
-          default : i.priorityClass = 'is-success'
+        i.formattedDueDate = DateTime.fromMillis(i.dueDate).toFormat('ff')
+        i.dueDateForParse = new Date(i.dueDate)
+        i.emergencyLevel = ''
+        const milliSecondsLeft = i.dueDate - Date.now()
+        const secondsLeft = Math.floor(milliSecondsLeft / 1000)
+        const minutesLeft = Math.floor(secondsLeft / 60)
+        const hoursLeft = Math.floor(minutesLeft / 60)
+        if (hoursLeft > 24) {
+          i.emergencyLevel = 'is-info'
+          i.emergencyLevelTitle = 'You are alright.'
+        } else if (hoursLeft <= 24 && hoursLeft > 12 && !i.isDone) {
+          i.emergencyLevel = 'is-warning'
+          i.emergencyLevelTitle = 'You need hurry.'
+        } else if (hoursLeft <= 12 && !i.isDone) {
+          i.emergencyLevel = 'is-danger'
+          i.emergencyLevelTitle = 'Do It! ASAP!'
+        }
+        if (i.isDone) {
+          i.emergencyLevel = 'is-success'
+          i.emergencyLevelTitle = 'You are done!'
         }
         return i
       })
